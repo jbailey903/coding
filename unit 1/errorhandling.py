236a8152b3d61603f083f2b55768a9fb9a116c39
jbailey903 <jbailey903@boyslatin.net>
2025-09-11 13:58:25 +0000

    test submission

---
+++ unit 1/errorhandling.py
@@ -0,0 +1,17 @@
+#type eror= (data type error)
+# has detected an issue with how you using
+# data types
+
+print (coding class is in room + str(105)
+ print int(100 +100) 
+
+ # syntax error=
+ # an issue with how your code is written
+ # somewhere you broke the grammer rules of the language 
+  
+ 10books =10
+
+ print (1+10books)
+ print (have a good day
+# arithmetic error= the computer has detected 
+# a math issue    
---
+++ unit 2/functions.py
@@ -0,0 +1,26 @@
+
+#function- is a set of code instructions labeld under
+# a custom name that the computer will run
+# function suntax (rules of how ita written)
+# functions have 2 phases: function defenition and
+# function call
+
+# functiom definition
+# we are describing the instruction for our custom code.
+# we are adding logic to the computers vocabulary
+
+def example ():
+print  (good morning) # 1 instruction : print good morning 
+
+
+
+# phase 2: function call
+# once we have the definition, we can now run the instructions
+# by writing the function name
+
+
+
+# we indent to infroe the computer that we are about to write
+# code instruction. if we dont we will get a error
+
+def exampple():
