9a2f2b6d1fd6a6897f480dfca56232d489677858
jbailey903 <jbailey903@boyslatin.net>
2025-09-17 14:30:04 +0000

    parameter

---
+++ unit 2/functiondata.py
@@ -0,0 +1,47 @@
+
+# how to create a function that passes data
+# step 1. create a function definition
+def bnbrefund(username,refundamount):
+    #print(sorry + useranme + for your cancellation
+     #print(we will refund $ + refundamount + amount to your bank)
+     # step 2 call the function / execute instructions
+
+     #bnbrefund (jacob,3000)
+
+
+
+     #print (my name is Jaquan + my birthday is + june 30 )
+     #1 instructionMy name is 
+     # 
+     # 
+     #
+     # 
+     # 
+     # 
+     # 
+    
+     #1Jaquan and my birthday is June 30
+#print My name is Jaquan and it is on June 30
+print ("My name" is + name  " and my bday is "+ bday)
+
+
+bday (Jaquan, June 30)
+
+# class activity 2
+# create a function that will convert dollars into pennies
+# your function should take the dollar amount in as a parameter 
+#1 dollar
+#100 pennies
+def(1 dollar = 100 pennies)
+def ("my" dollar is equivelent to 100 pennies)
+
+dollarconverter (25)
+length=20
+#width=15
+length with 0.5
+length = 40
+width = 20
+
+
+
+def tempconverter(far):
