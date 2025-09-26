9cd2e66f40826efff162f3dd8b790d55f029aeac
jbailey903 <jbailey903@boyslatin.net>
2025-09-26 00:29:09 +0000

    classwork/homework

--- readme.md
+++
@@ -1,2 +0,0 @@
-Jaquan Bailey
-#
---
+++ unit 2/conditions.py
@@ -0,0 +1,43 @@
+# conditional statements- code instruction that
+# have different outcomes based on the inputted data
+
+
+# conditional statement syntax
+# if keyword- controls the conditiona we want to satisfy
+
+# else keyword- our default outcome. The thing that
+# happens when our conditions are not satisfied
+
+# weather = input("what is the weather like today?")
+#if weather == "sunny":
+  #     print("it is beautiful outside. Bring some sun glasses")
+   #    print ("I cant tell you the weather, but have a good day")
+                        
+
+# password== weather
+print ('welcome you are logged in')                 
+print ('sorry, incorrect password try again')
+
+#def icecream():
+ #      if down == 1 and yards <=5
+  #            print("throw the ball")
+   #    elif down == 2 and yards <=5:
+   #           ("throw the ball")
+    #   elif down ==3 and yards <=5:
+     #         print("run the ball to get extra yards")
+      # else #this is the exist; assume it is 4th done 
+       #       print("punt")
+
+def testakerpermit (age):
+       if age> 16:
+              print("you may take your permit test")
+       else:  
+              print('you are not old enough') 
+
+testakerpermit(18)
+
+def positiveornegativenumber(5):
+       print("im trying to determine if 5 is a positive or negative number")
+       if positive>5:
+       else:
+      print("The number 5 is a positive number not negative")        
