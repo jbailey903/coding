d429ad9cf47c6815d7912439d3a93926d502e73b
jbailey903 <jbailey903@boyslatin.net>
2025-10-30 14:26:23 +0000

    loop activity

---
+++ unit 3 /while.loops.py
@@ -0,0 +1,26 @@
+#while loop defintion- a while loop is a type of construct
+# where code instruction will keep on running so
+# long as a condition is TRUE(boolean)
+
+# Note- in order to stop a loop(or any program)from running
+# in your terminal,click crtl+c at the smae time
+
+# while loop syntax
+
+ageVerification=17
+purchasersAge=15
+
+def password():
+    savepassword='123abc'
+    userpassword=input ("please try again")
+
+
+
+
+
+    timer=30
+while timer >0: #true
+        timer-=1
+print(timer)
+else:
+print('done')
---
+++ unit 3 /whileloopactivity.py
@@ -0,0 +1,39 @@
+# you need a list for your numbers
+# stop the loop when you enter quit
+
+def x():
+    i = 1
+    while i < 6:
+        print(i)
+        input("6")
+        i+=6
+else:
+print("i is no longer less than 6")
+x()
+
+
+def numberloop():
+    numbers=[]
+    usernumber=input("pkease type 9in a number")
+    while usernumber ='quit':
+transfornumber=int (user number)
+numbers.append (transfornumber)
+print(numbers)
+usernumber= input ("please type in a n umber")
+else:
+print("loop has stopped")
+
+numberLoop()
+
+
+
+
+def guessthenumber():
+    correctnumner=8
+    userguess=input ('guess the correct number')
+    while user=guess=correctnumber:
+userguess=input('guess the correct number')
+else:
+print("congrats you guessed the correct number")
+
+guessthenumber()
