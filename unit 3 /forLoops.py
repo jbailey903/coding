91ce89a459df34c1078d059d67acbde6fb290284
jbailey903 <jbailey903@boyslatin.net>
2025-11-06 15:26:47 +0000

    for loops

---
+++ unit 3 /forLoops.py
@@ -0,0 +1,40 @@
+#For loops- A type of construct that rnus code instruction
+a# a finite amount over a group of data
+
+halloweenBag = ['snickers, hershey bar, twizzler, candy apple']
+
+# i is a variable and is short for iterator
+for candy in halloweenBag:
+print(candy)
+print('I got this candy in my bag' + candy).,    
+
+
+def tf():
+        for x in range (3):
+                print('true or false: 3 is greatger than 2')
+                answer=input()
+                if answer!='true'
+                print('wrong, try again')
+                print(' attempt + str(x)')
+
+
+                for x (range)(5):
+                word= input ("please type in a word")
+                print (word)
+
+
+
+
+
+                #looping through strings
+                word= "python"
+                for letter in word:
+                        
+
+
+
+                        #looping through list of numbers
+                        shoopingprices 
+                        total=0
+
+                        for totals in shoopingprices:
