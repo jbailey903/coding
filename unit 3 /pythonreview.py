27bed036bd8f5238a0403b2478594581f11a82f1
jbailey903 <jbailey903@boyslatin.net>
2025-11-10 15:30:34 +0000

    pythonreview

---
+++ unit 3 /pythonreview.py
@@ -0,0 +1,8 @@
+1.x= 5
+print(students have 5 homework and classwork assignments in a week)
+if>5
+    
+2. def output(x):
+return x{output argument}
+mytext= output("how would this text look if it was a backwards output")
+print(mytext)
